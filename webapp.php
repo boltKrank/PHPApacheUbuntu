6e56b65c4a11c749fa13bfea8c8c48c78054b621
Simon Anderson <simon.anderson111@gmail.com>
2018-07-18 13:37:40 +0900

    change heading

--- webapp.php
+++ webapp.php
@@ -17,11 +17,11 @@
     $data = explode(',', explode(' up', shell_exec('uptime'))[1]);
     return $data[0];
   }
-  
+
   $ip_client = getClientIP();
   $uptime = getUptime();
-  
-  echo "Hello World! You have successfully deployed your PHP application to your Apache server.<br><br>";
+
+  echo "Hello People! You have successfully deployed your PHP application to your Apache server.<br><br>";
   echo "Server Uptime: $uptime <br>";
   echo "Your IP: $ip_client <br><br>";
   phpinfo();
